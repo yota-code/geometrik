f9cbb5024953f81ab802ead02997111af850c949
yoochan <yota.news@gmail.com>
2021-06-01 16:05:57 +0200

    plot moved to globe because

--- package/geometrik/threed/__init__.py
+++ package/geometrik/threed/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import geometrik.threed.vector as vector
-import geometrik.threed.plane as plane
-import geometrik.threed.plot as plot
+# import geometrik.threed.vector as vector
+# import geometrik.threed.plane as plane
+# import geometrik.threed.plot as plot
 
 from geometrik.threed.vector import Vector
 from geometrik.threed.plane import Plane
-from geometrik.threed.plot import UnitSpherePlot
+# from geometrik.threed.plot import UnitSpherePlot
