19d7a59282948ffb2c1761ce1e27b39ea4891185
yoochan <yota.news@gmail.com>
2021-07-16 18:09:36 +0200

    eow

--- package/geometrik/threed/__init__.py
+++ package/geometrik/threed/__init__.py
@@ -3,8 +3,8 @@
 from geometrik.threed.vector import Vector
 from geometrik.threed.plane import Plane
 
-v_null = Vector(0.0, 0.0, 0.0)
-v_north = Vector(0.0, 0.0, 1.0)
-v_east = Vector(0.0, 1.0, 0.0)
+v_null = Vector(0.0, 0.0, 0.0, True)
+v_north = Vector(0.0, 0.0, 1.0, True)
+v_east = Vector(0.0, 1.0, 0.0, True)
 v_down = v_north @ v_east
 v_up = v_east @ v_north
--- package/geometrik/threed/plane.py
+++ package/geometrik/threed/plane.py
@@ -7,61 +7,55 @@ import sympy
 import geometrik.threed as g3d
 
 class Plane() :
-	# define a plane
-	def __init__(self, normal: g3d.vector.Vector, point=None) :
-		self.normal = normal
 
-	def distance(self, point) :
-		u = point - self.point
-		n = self.director
+	# define a plane which pass through the point (0, 0), defined by its normal vector
+	def __init__(self, normal: g3d.Vector) :
+		self.normal = normal.normalized()
 
-		return (n * u) / n.norm
+	def distance(self, point: g3d.Vector) :
+		return (self.normal * point) / self.normal.norm
 
 	def is_on_plane(self, point) :
 		""" return True if the point lies on the plane """
 		return math.isclose( self.distance(point), 0.0 )
 
 	def project(self, vector) :
-		return vector - ( vector * self.director * self.director )
+		return vector - ( vector * (self.normal * self.normal) )
 
-	def frame(self, other=None) :
-		""" return a frame where z is oriented toward other:
+	def frame(self, other=g3d.Vector(0.0, 0.0, 1.0, True)) :
+		""" return a frame where z is oriented toward other (by default north):
 
 			* no solution is returned if self and other are colinears
 			* if other is not specified, v_north is used as other
 			* in this case, the frame is not defined at poles
-		
 		"""
+
 		if other is None :
-			other = g3d.vector.v_north
+			""" return an arbitrarily oriented frame optimized to reduce numerical errors,
+			the frame can be defined everywhere """
+			c_tpl = self.normal.as_tuple
+			c_max = max(range(len(c_tpl)), key=lambda i: c_tpl[i])
 
-		x = self.normal
-		y = ( g3d.vector.v_north @ x ).normalized()
-		z = x @ y
+			c_lst = list(c_tpl)
+			c_lst[c_max] = 0
+
+			num = sum(c_lst)
+			den = c_tpl[c_max]
+
+			y_lst = [1.0, 1.0, 1.0]
+			y_lst[c_max] = - num / den
+
+			x = self.normal
+			y = g3d.Vector(* y_lst).normalized()
+			z = x @ y
+
+		else :
+			x = self.normal
+			y = (other @ x).normalized()
+			z = x @ y
 
 		return y, z
-
-	def frame_optimal(self) :
-		""" return an arbitrary frame optimized for numerical errors, the frame can be defined everywhere """
-
-		c_tpl = self.normalized().as_tuple
-		c_max = max(range(len(c_tpl)), key=lambda i: c_tpl[i])
-
-		c_lst = list(c_tpl)
-		c_lst[c_max] = 0
-
-		num = sum(c_lst)
-		den = c_tpl[c_max]
-
-		y_lst = [1.0, 1.0, 1.0]
-		y_lst[c_max] = - num / den
-
-		x = self.normalized()
-		y = g3d.vector.Vector(* y_lst).normalized()
-		z = x @ y
-		return y, z
-
-
+		
 # class GenericPlane() :
 # 	def __init__(self, normal, point=None) :
 # 		self.normal = normal
