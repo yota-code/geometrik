641fc3cd07ff481d9153377db02977c6b04f01c1
yoochan <yota.news@gmail.com>
2022-10-21 00:25:28 +0200

    proper split of math and sympy

--- package/geometrik/threed/vector.py
+++ package/geometrik/threed/vector.py
@@ -5,12 +5,15 @@ import sympy
 
 class Vector() :
 
-	def __init__(self, x, y, z, is_unit=False) :
+	def __init__(self, x, y, z, is_unit=False, is_symbolic=False) :
 		self.x = x
 		self.y = y
 		self.z = z
 
 		self._is_unit = is_unit
+
+		self._is_symbolic = is_symbolic
+		self.m = sympy if is_symbolic else math
 
 	@property
 	def as_tuple(self) :
@@ -19,14 +22,12 @@ class Vector() :
 	@staticmethod
 	def symbolic(name) :
 		Vx, Vy, Vz = sympy.symbols(' '.join(f'{name}_{i}' for i in 'xyz'))
-		return Vector(Vx, Vy, Vz)
+		return Vector(Vx, Vy, Vz, is_symbolic=True)
 
 	def __repr__(self) :
-		try :
-			return f"{self.__class__.__name__}({self.x:0.3g}, {self.y:0.3g}, {self.z:0.3g})"
-		except TypeError :
-			s = f"{self.__class__.__name__}({sympy.latex(self.x)}, {sympy.latex(self.y)}, {sympy.latex(self.z)})"
-			return s
+		if self._is_symbolic :
+			return f"{self.__class__.__name__}({sympy.latex(self.x)}, {sympy.latex(self.y)}, {sympy.latex(self.z)})"
+		return f"{self.__class__.__name__}({self.x:0.3g}, {self.y:0.3g}, {self.z:0.3g})"
 
 	def subs(self, v_map) :
 		v_lst = self.x, self.y, self.z
@@ -42,23 +43,25 @@ class Vector() :
 	def __iter__(self) :
 		return (i for i in (self.x, self.y, self.z))
 
-	@staticmethod
-	def compose(a, b, theta) :
-		return math.cos(theta) * a + math.sin(theta) * b
-
-	def deviate(self, other, theta) :
-		return math.cos(theta) * self + math.sin(theta) * other
+	def deflect(self, other, theta) :
+		return self.m.cos(theta) * self + self.m.sin(theta) * other
 				
 	def __add__(self, other) :
-		if isinstance(other, Vector) :
-			return Vector(
-				self.x + other.x,
-				self.y + other.y,
-				self.z + other.z
-			)
+		return Vector(
+			self.x + other.x,
+			self.y + other.y,
+			self.z + other.z,
+			is_symbolic = self._is_symbolic or other._is_symbolic
+		)
 	
 	def __sub__(self, other) :
-		return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+		return self + (- other)
+		Vector(
+			self.x - other.x,
+			self.y - other.y,
+			self.z - other.z,
+			is_symbolic = self._is_symbolic or other._is_symbolic
+		)
 			
 	def __mul__(self, other) :
 		if isinstance(other, Vector) :
@@ -72,7 +75,12 @@ class Vector() :
 	__rmul__ = __mul__
 
 	def __neg__(self) :
-		return Vector(-self.x, -self.y, -self.z)
+		return Vector(
+			-self.x,
+			-self.y,
+			-self.z,
+			is_symbolic = self._is_symbolic
+		)
 	
 	def __truediv__(self, other) :
 		# print("__truediv__({0}, {1})".format(type(self), type(other)))
@@ -91,10 +99,15 @@ class Vector() :
 			return self.vector_product(other)
 		else :
 			raise ValueError("operation not implemented for this type: {0!r}".format(other))
-			
-	def lambda_product(self, other) :
+	
+	def lambda_product(self, k:float) :
 		""" constant * vector product """
-		return Vector(other * self.x, other * self.y, other * self.z)
+		return Vector(
+			k * self.x,
+			k * self.y,
+			k * self.z,
+			is_symbolic = self._is_symbolic
+		)
 				
 	def scalar_product(self, other) :
 		""" scalar product """
@@ -106,6 +119,7 @@ class Vector() :
 			self.y * other.z - self.z * other.y,
 			self.z * other.x - self.x * other.z,
 			self.x * other.y - self.y * other.x,
+			is_symbolic = self._is_symbolic or other._is_symbolic
 		)
 		
 	def __str__(self) :
@@ -113,39 +127,44 @@ class Vector() :
 		
 	@property
 	def norm(self) :
-		try :
-			return math.sqrt(self.norm_2)
-		except TypeError :
-			return sympy.sqrt(self.norm_2)
+		return self.m.sqrt(self.norm_2)
 
 	@property
 	def norm_2(self) :
 		return self.scalar_product(self)
 		
 	def normalized(self) :
-		if self._is_unit :
-			return self
-		else :
-			n = self.norm
-			return Vector(self.x / n, self.y / n, self.z / n, True)
+		n = self.norm
+
+		return Vector(
+			self.x / n,
+			self.y / n,
+			self.z / n,
+			is_unit=True,
+			is_symbolic=self._is_symbolic
+		)
 
 	def atan2(self, Fy, Fx) :
-		return math.atan2(Fy * self, Fx * self)
+		return self.m.atan2(Fy * self, Fx * self)
 
-	def angle_to(self, other, sign=None) :
-		if sign is None :
-			c = (self * other) / (
-				(1 if self._is_unit else self.norm) *
-				(1 if self._is_unit else other.norm)
-			)
-			try :
-				return math.acos(max(-1.0, min(c, 1.0)))
-			except TypeError :
-				return sympy.acos(c)
+	def angle_to(self, other, way=None) :
+		# part of the formula for the angle between two vectors
+		ca = (self * other) / (self.norm * other.norm)
+
+		# if the result is numeric, cap it in [-1.0 ; 1.0] for the acos to come
+		cb = max(-1.0, min(ca, 1.0)) if not ca._is_symbolic else ca
+
+		# last arccos
+		cc = self.m.acos(cb)
+
+		if way is None :
+			s = (self @ other) * way
+			if cc._is_symbolic :
+				return sympy.sign(s) * cc
+			else :
+				return math.copysign(cc, s)
 		else :
-			c = (self * other) / (self.norm * other.norm)
-			s = (self @ other) * sign
-			return math.copysign(math.acos(max(-1.0, min(c, 1.0))), s)
+			return cc
 
 	def frame(self, other=None) :
 		""" return a frame where z is oriented toward other (by default north):
@@ -181,5 +200,8 @@ class Vector() :
 
 		return y, z # east, north
 
-	def project(self, normal) :
-		return self - ( (self * normal) * normal )
+	def project_normal(self, normal) :
+		return self - self.project_tangent(normal)
+
+	def project_tangent(self, tangent) :
+		return ((self * tangent) * tangent)
